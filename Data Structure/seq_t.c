d2fba0277e29987c4e15a3483648516bae97c6fe
changgang <32517410+jacksparrowwang@users.noreply.github.com>
2017-12-17 15:49:20 +0800

    数据结构静态顺序表的完善
    增加，删除多个元素，删除指定位置的元素，获取元素的个数，判断顺序表是否为空，
    采用冒泡进行排序，和回掉函数实现冒泡排序

--- Data Structure/seq_t.c
+++ Data Structure/seq_t.c
@@ -99,7 +99,7 @@ void update_element_of_anywhere_address(seqlist *seq,size_t address,TYPECHAR val
 	seq->seq_t[address] = value;
 }
 
-void gei_yuansu_chazhao_xiabiao(seqlist *seq,TYPECHAR value) // 给元素查找下标subscript  value 为所给元素
+size_t gei_yuansu_chazhao_xiabiao(seqlist *seq,TYPECHAR value) // 给元素查找下标subscript  value 为所给元素
 {
 	size_t i = 0;
 	assert(seq);
@@ -108,13 +108,14 @@ void gei_yuansu_chazhao_xiabiao(seqlist *seq,TYPECHAR value) // 给元素查找下标su
 		if(seq->seq_t[i] == value)
 		{
 			printf("查找元素下标为：%d\n",i);
-			break;
+			return i;
 		}
 	}
 	if(i==seq->size)
 	{
 		printf("未查找到\n");	
 	}
+	return -1;
 }
 
 
@@ -135,21 +136,146 @@ void insert_element_of_anywhere_address(seqlist *seq,size_t address,TYPECHAR val
 	seq->seq_t[i] = value;
 }
 
+//删除指定位置的元素
+void seqListErase_address(seqlist* seq, size_t pos)
+{
+	size_t i = 0;
+	assert(seq);
+	if(pos > seq->size)
+	{
+		printf("未查找到\n");
+		return ;
+	}
+	for(i=pos; i < seq->size; i++)
+	{
+		seq->seq_t[i] = seq->seq_t[i+1];
+	}
+	(seq->size)--;
+	return ;
+}
+
+//按照指定的值来删除,如果重复只删除第一个。时间复杂度为O（n）
+void SeqListRemove(seqlist *seq, TYPECHAR to_delete)
+{	
+	size_t i = 0;
+	assert(seq);
+	i = gei_yuansu_chazhao_xiabiao(seq,to_delete);
+	seqListErase_address(seq,i);
+	return ;
+}
+
+//按照指定值来删除，表中所有要删除的值，时间复杂度为O（n）
+void SeqListRemoveAll(seqlist* seq, TYPECHAR to_delete)
+{
+	size_t count = 0;
+	size_t i = 0;
+	assert(seq);
+	for(i = 0; i < seq->size; i++)
+	{
+		if(seq->seq_t[i] != to_delete)
+		{
+			seq->seq_t[i-count] = seq->seq_t[i];
+		}
+		else
+		{
+			count++;
+		}
+	}
+	seq->size-=count;
+	return ;
+}
+
+
+//获取顺序表中元素的个数
+size_t SeqListSize(seqlist* seq)
+{
+	assert(seq);
+	return seq->size;
+}
+
+//判断顺序表是否为空,为空返回1，不为空返回0。
+int SeqListEmpty(seqlist* seq)
+{
+	assert(seq);
+	if (seq->size == 0)
+	{
+		return 1;
+	}
+		return 0;
+}
+
+
+//交换
+void swap(char *p, char *q)
+{
+	char temp = *p;
+	*p = *q;
+	*q = temp;
+}
+
+//冒泡排序
+void SeqListBubbleSort(seqlist* seq)
+{
+	size_t i = 0;
+	size_t j = 0;
+	assert(seq);
+	for(i = 0; i < seq->size-1; i++)
+	{
+		for(j = 0; j < seq->size-i-1; j++)
+		{
+			if(seq->seq_t[j] > seq->seq_t[j+1])
+			{
+				swap(&(seq->seq_t[j]),&(seq->seq_t[j+1]));
+			}
+		}
+	}
+}
+
+//回掉函数
+int cmp(TYPECHAR value1,TYPECHAR value2)
+{
+	return value1 - value2 ;
+}
+
+//回掉函数冒泡排序
+void SeqListBubbleSortEx(seqlist* seq, INT (*cmp)(TYPECHAR , TYPECHAR))
+{
+	size_t i = 0;
+	size_t j = 0;
+	assert(seq);
+	for (i = 0; i < seq->size-1; i++)
+	{
+		for (j = 0; j < seq->size-i-1; j++)
+		{
+			if (cmp(seq->seq_t[j],seq->seq_t[j+1]) >0)
+				swap(&(seq->seq_t[j]),&(seq->seq_t[j+1]));
+		}
+	}
+}
+
 
 int main()
 {
 	initlist(&seq);
-	//end_push_list(&seq,'a');
-	//end_push_list(&seq,'b');
-	//end_push_list(&seq,'c');
+	end_push_list(&seq,'a');
+	end_push_list(&seq,'b');
+	end_push_list(&seq,'c');
+	end_push_list(&seq,'b');
 	//end_pop_list(&seq);
 	//frist_push_list(&seq,'d');
 	//frist_push_list(&seq,'e');
 	//frist_pop_list(&seq);
-	//read_element_of_anywhere_address(&seq,0);
-	//update_element_of_anywhere_address(&seq,2,'q');
-	//gei_yuansuh_shibie_xiabiao(&seq,'b');
-	//insert_element_of_anywhere_address(&seq,0,'w');
+	//read_element_of_anywhere_address(&seq,0);//查找下表的元素
+	//update_element_of_anywhere_address(&seq,2,'q');//修改指定位置的元素
+	//gei_yuansuh_shibie_xiabiao(&seq,'b');//给出元素查找出元素的下表
+	//insert_element_of_anywhere_address(&seq,0,'w');//在任意位置插入元素
+	//seqListErase_address(&seq,4);//删除指定位置的元素
+	//SeqListRemove(&seq,'b');//删除所给值的顺序表中的第一个元素
+	//SeqListRemoveAll(&seq,'c');//删除所给指定值的所有元素
+	//printf("%lu",SeqListSize(&seq));//计算顺序表中的个数
+	//printf("%d",SeqListEmpty(&seq));//判断空顺序表
+	//SeqListBubbleSort(&seq);//冒泡排序
+	//SeqListBubbleSortEx(&seq, cmp);//回掉函数冒泡排序的测试
 	show(&seq);
 	return 0;
 }
--- Data Structure/sequence.h
+++ Data Structure/sequence.h
@@ -4,6 +4,8 @@
 #define TYPECHAR char
 #define SHOWTYPE "%c"
 
+typedef int INT;
+
 typedef struct seqlist
 {
 	size_t size ;
@@ -15,12 +17,48 @@ seqlist seq ;
 
 void show(seqlist *seq);
 void initlist(seqlist *seq);
+
+//尾部插入一个元素
 void end_push_list(seqlist *seq,TYPECHAR value);
+
+//尾部删除一个元素
 void end_pop_list(seqlist *seq);
+
+//首部插入一个元素
 void frist_push_list(seqlist *seq,TYPECHAR value);
+
+//在顺序表首部删除一个元素
 void frist_pop_list(seqlist *seq);
+
+//读取任意位置的元素。address为位置。
 void read_element_of_anywhere_address(seqlist *seq,size_t address);
+
+//修改指定位置的元素。address为修改的下标。value为修改成的值。
 void update_element_of_anywhere_address(seqlist *seq,size_t address,TYPECHAR value);
+
+// 给元素查找下标subscript  value 为所给元素
 void gei_yuansuh_shibie_xiabiao(seqlist *seq,TYPECHAR value);
+
+//在任意位置插入一个元素。address插入的位置。value要插入的值。
 void insert_element_of_anywhere_address(seqlist *seq,size_t address,TYPECHAR value);
 
+//删除指定位置的值
+void seqListErase_address(seqlist *seq, size_t pos);
+
+//按照指定值来删除，如果重复只删除第一个。
+void SeqListRemove(seqlist* seq, TYPECHAR to_delete); 
+
+//按照指定值来删除，表中所有要删除的值，时间复杂度为O（n）
+void SeqListRemoveAll(seqlist* seq, TYPECHAR to_delete);
+
+//获取顺序表中的元素
+size_t SeqListSize(seqlist* seq);
+
+//判断顺序表是否为空,为空返回1，不为空返回0。
+int SeqListEmpty(seqlist* seq);
+
+//冒泡排序
+void SeqListBubbleSort(seqlist* seq); 
+
+//回掉函数冒泡排序
+void SeqListBubbleSortEx(seqlist* seq, INT (*cmp)(TYPECHAR , TYPECHAR));
